dghubble/oauth1

commit b3a9facaf291893fe0e834b5386438b57dc51342

Update names and doc comments for golint compliance

sign.go
@@ -17,24 +17,24 @@ import (
 )
 
 const (
-	AUTHORIZATION_HEADER     = "Authorization"
-	AUTHORIZATION_PREFIX     = "OAuth " // trailing space is intentional
-	OAUTH_CONSUMER_KEY       = "oauth_consumer_key"
-	OAUTH_NONCE              = "oauth_nonce"
-	OAUTH_SIGNATURE          = "oauth_signature"
-	OAUTH_SIGNATURE_METHOD   = "oauth_signature_method"
-	OAUTH_TIMESTAMP          = "oauth_timestamp"
-	OAUTH_TOKEN              = "oauth_token"
-	OAUTH_VERSION            = "oauth_version"
-	OAUTH_CALLBACK           = "oauth_callback"
-	OAUTH_VERIFIER           = "oauth_verifier"
-	DEFAULT_SIGNATURE_METHOD = "HMAC-SHA1"
-	DEFAULT_VERSION          = "1.0"
-	contentType              = "Content-Type"
-	formContentType          = "application/x-www-form-urlencoded"
+	authorizationHeaderKey    = "Authorization"
+	authorizationPrefix       = "OAuth " // trailing space is intentional
+	oauthConsumerKeyParam     = "oauth_consumer_key"
+	oauthNonceParam           = "oauth_nonce"
+	oauthSignatureParam       = "oauth_signature"
+	oauthSignatureMethodParam = "oauth_signature_method"
+	oauthTimestampParam       = "oauth_timestamp"
+	oauthTokenParam           = "oauth_token"
+	oauthVersionParam         = "oauth_version"
+	oauthCallbackParam        = "oauth_callback"
+	oauthVerifierParam        = "oauth_verifier"
+	defaultSignatureMethod    = "HMAC-SHA1"
+	defaultOauthVersion       = "1.0"
+	contentType               = "Content-Type"
+	formContentType           = "application/x-www-form-urlencoded"
 )
 
-// Handles signing requests and setting authorization header
+// Signer handles signing requests and setting the authorization header.
 type Signer struct {
 	config *Config
 }
@@ -43,13 +43,13 @@ type Signer struct {
 // request (temporary credential) according to RFC 5849 2.1.
 func (s *Signer) SetRequestTokenAuthHeader(req *http.Request) error {
 	oauthParams := basicOAuthParams(s.config.ConsumerKey)
-	oauthParams[OAUTH_CALLBACK] = s.config.CallbackURL
+	oauthParams[oauthCallbackParam] = s.config.CallbackURL
 	signatureBase, err := signatureBase(req, oauthParams)
 	if err != nil {
 		return err
 	}
 	signature := signature(s.config.ConsumerSecret, "", signatureBase)
-	oauthParams[OAUTH_SIGNATURE] = signature
+	oauthParams[oauthSignatureParam] = signature
 	setAuthorizationHeader(req, oauthParams)
 	return nil
 }
@@ -58,14 +58,14 @@ func (s *Signer) SetRequestTokenAuthHeader(req *http.Request) error {
 // (token credential) according to RFC 5849 2.3.
 func (s *Signer) SetAccessTokenAuthHeader(req *http.Request, requestToken *RequestToken, verifier string) error {
 	oauthParams := basicOAuthParams(s.config.ConsumerKey)
-	oauthParams[OAUTH_TOKEN] = requestToken.Token
-	oauthParams[OAUTH_VERIFIER] = verifier
+	oauthParams[oauthTokenParam] = requestToken.Token
+	oauthParams[oauthVersionParam] = verifier
 	signatureBase, err := signatureBase(req, oauthParams)
 	if err != nil {
 		return err
 	}
 	signature := signature(s.config.ConsumerSecret, requestToken.TokenSecret, signatureBase)
-	oauthParams[OAUTH_SIGNATURE] = signature
+	oauthParams[oauthSignatureParam] = signature
 	setAuthorizationHeader(req, oauthParams)
 	return nil
 }
@@ -74,13 +74,13 @@ func (s *Signer) SetAccessTokenAuthHeader(req *http.Request, requestToken *Reque
 // requests with an AccessToken (token credential) according to RFC 5849 3.1.
 func (s *Signer) SetRequestAuthHeader(req *http.Request, accessToken *Token) error {
 	oauthParams := basicOAuthParams(s.config.ConsumerKey)
-	oauthParams[OAUTH_TOKEN] = accessToken.Token
+	oauthParams[oauthTokenParam] = accessToken.Token
 	signatureBase, err := signatureBase(req, oauthParams)
 	if err != nil {
 		return err
 	}
 	signature := signature(s.config.ConsumerSecret, accessToken.TokenSecret, signatureBase)
-	oauthParams[OAUTH_SIGNATURE] = signature
+	oauthParams[oauthSignatureParam] = signature
 	setAuthorizationHeader(req, oauthParams)
 	return nil
 }
@@ -89,26 +89,26 @@ func (s *Signer) SetRequestAuthHeader(req *http.Request, accessToken *Token) err
 // excluding the oauth_signature parameter.
 func basicOAuthParams(consumerKey string) map[string]string {
 	return map[string]string{
-		OAUTH_CONSUMER_KEY:     consumerKey,
-		OAUTH_SIGNATURE_METHOD: DEFAULT_SIGNATURE_METHOD,
-		OAUTH_TIMESTAMP:        strconv.FormatInt(epoch(), 10),
-		OAUTH_NONCE:            nonce(),
-		OAUTH_VERSION:          DEFAULT_VERSION,
+		oauthConsumerKeyParam:     consumerKey,
+		oauthSignatureMethodParam: defaultSignatureMethod,
+		oauthTimestampParam:       strconv.FormatInt(epoch(), 10),
+		oauthNonceParam:           nonce(),
+		oauthVersionParam:         defaultOauthVersion,
 	}
 }
 
 // setAuthorizationHeader formats the OAuth1 protocol parameters into a header
 // and sets the header on the Request.
 func setAuthorizationHeader(req *http.Request, oauthParams map[string]string) {
-	authHeader := authorizationHeader(oauthParams)
-	req.Header.Set(AUTHORIZATION_HEADER, authHeader)
+	authHeader := getAuthHeader(oauthParams)
+	req.Header.Set(authorizationHeaderKey, authHeader)
 }
 
-// authorizationHeader combines the OAuth1 protocol parameters into an
-// authorization header according to RFC 5849 3.5.1 and returns it.
-// The oauthParams should include the "oauth_signature" key/value pair.
+// getAuthHeader combines the OAuth1 protocol parameters into an authorization
+// header according to RFC 5849 3.5.1 and returns it. The oauthParams should
+// include the "oauth_signature" key/value pair.
 // Does not mutate the oauthParams.
-func authorizationHeader(oauthParams map[string]string) string {
+func getAuthHeader(oauthParams map[string]string) string {
 	// percent encode
 	params := map[string]string{}
 	for key, value := range oauthParams {
@@ -121,7 +121,7 @@ func authorizationHeader(oauthParams map[string]string) string {
 		pairs[i] = fmt.Sprintf("%s=%s", key, value)
 		i++
 	}
-	return AUTHORIZATION_PREFIX + strings.Join(pairs, ", ")
+	return authorizationPrefix + strings.Join(pairs, ", ")
 }
 
 // signatureBase combines the uppercase request method, percent encoded base
@@ -130,7 +130,7 @@ func authorizationHeader(oauthParams map[string]string) string {
 // Does not mutate the Request or basicOAuthParams.
 func signatureBase(req *http.Request, basicOAuthParams map[string]string) (string, error) {
 	method := strings.ToUpper(req.Method)
-	baseUrl := strings.Split(req.URL.String(), "?")[0]
+	baseURL := strings.Split(req.URL.String(), "?")[0]
 	// add oauth, query, and body parameters into params
 	params := map[string]string{}
 	for key, value := range req.URL.Query() {
@@ -158,7 +158,7 @@ func signatureBase(req *http.Request, basicOAuthParams map[string]string) (strin
 	}
 	// encode params into a parameter string (RFC5849 3.4.1.3, 3.4.1.3.2)
 	parameterString := encodeParams(params)
-	baseParts := []string{method, PercentEncode(baseUrl), PercentEncode(parameterString)}
+	baseParts := []string{method, PercentEncode(baseURL), PercentEncode(parameterString)}
 	return strings.Join(baseParts, "&"), nil
 }
 
@@ -174,7 +174,7 @@ func encodeParams(unencodedParams map[string]string) string {
 	// sort by key
 	keys := make([]string, len(params))
 	i := 0
-	for key, _ := range params {
+	for key := range params {
 		keys[i] = key
 		i++
 	}

config.go
@@ -8,8 +8,8 @@ import (
 )
 
 const (
-	OAUTH_TOKEN_SECRET       = "oauth_token_secret"
-	OAUTH_CALLBACK_CONFIRMED = "oauth_callback_confirmed"
+	oauthTokenSecretParam       = "oauth_token_secret"
+	oauthCallbackConfirmedParam = "oauth_callback_confirmed"
 )
 
 // Config represents an OAuth1 consumer's (client's) credentials, callback URL,
@@ -25,6 +25,7 @@ type Config struct {
 	Endpoint Endpoint
 }
 
+// NewConfig returns a new Config with the given consumer key and secret.
 func NewConfig(consumerKey, consumerSecret string) *Config {
 	return &Config{
 		ConsumerKey:    consumerKey,
@@ -37,7 +38,7 @@ func (c *Config) Client(t *Token) *http.Client {
 	return NewClient(c, t)
 }
 
-// Returns a new http Client which signs requests via OAuth1.
+// NewClient returns a new http Client which signs requests via OAuth1.
 func NewClient(config *Config, token *Token) *http.Client {
 	transport := &Transport{
 		source: &ReuseTokenSource{token, nil},
@@ -82,11 +83,11 @@ func (c *Config) GetRequestToken() (*RequestToken, error) {
 	if err != nil {
 		return nil, err
 	}
-	if values.Get(OAUTH_CALLBACK_CONFIRMED) != "true" {
+	if values.Get(oauthCallbackConfirmedParam) != "true" {
 		return nil, errors.New("oauth_callback_confirmed was not true")
 	}
-	token := values.Get(OAUTH_TOKEN)
-	tokenSecret := values.Get(OAUTH_TOKEN_SECRET)
+	token := values.Get(oauthTokenParam)
+	tokenSecret := values.Get(oauthTokenSecretParam)
 	if token == "" || tokenSecret == "" {
 		return nil, errors.New("GetRequestToken response missing oauth token or secret")
 	}
@@ -104,7 +105,7 @@ func (c *Config) AuthorizationURL(rt *RequestToken) (*url.URL, error) {
 		return nil, err
 	}
 	values := authorizationURL.Query()
-	values.Add(OAUTH_TOKEN, rt.Token)
+	values.Add(oauthTokenParam, rt.Token)
 	authorizationURL.RawQuery = values.Encode()
 	return authorizationURL, nil
 }
@@ -121,8 +122,8 @@ func (c *Config) HandleAuthorizationCallback(req *http.Request) (tokenKey, verif
 	if err != nil {
 		return "", "", err
 	}
-	tokenKey = req.Form.Get(OAUTH_TOKEN)
-	verifier = req.Form.Get(OAUTH_VERIFIER)
+	tokenKey = req.Form.Get(oauthTokenParam)
+	verifier = req.Form.Get(oauthVerifierParam)
 	if tokenKey == "" || verifier == "" {
 		return "", "", errors.New("callback did not receive an oauth_token or oauth_verifier")
 	}
@@ -157,8 +158,8 @@ func (c *Config) GetAccessToken(requestToken *RequestToken, verifier string) (*T
 	if err != nil {
 		return nil, err
 	}
-	token := values.Get(OAUTH_TOKEN)
-	tokenSecret := values.Get(OAUTH_TOKEN_SECRET)
+	token := values.Get(oauthTokenParam)
+	tokenSecret := values.Get(oauthTokenSecretParam)
 	if token == "" || tokenSecret == "" {
 		return nil, errors.New("GetAccessToken response missing oauth token or secret")
 	}

token.go
@@ -7,6 +7,7 @@ type Token struct {
 	TokenSecret string
 }
 
+// NewToken returns a new Token with the given token and token secret.
 func NewToken(token, tokenSecret string) *Token {
 	return &Token{
 		Token:       token,
@@ -27,6 +28,7 @@ type ReuseTokenSource struct {
 	source TokenSource
 }
 
+// Token returns a Token or an error.
 func (ts *ReuseTokenSource) Token() (*Token, error) {
 	return ts.token, nil
 }

transport.go
@@ -12,6 +12,8 @@ type Transport struct {
 	signer *Signer
 }
 
+// RoundTrip OAuth1 signs the request with the source's access token and sets
+// the authorization header.
 func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	accessToken, err := t.source.Token()
 	if err != nil {

commit 531605b4a6baeb8b94ce06f69200c2f53d5d5db4

Fix incorrect header key in access token request auth header
* Fixes bug introduced in b3a9fac

sign.go
@@ -17,7 +17,7 @@ import (
 )
 
 const (
-	authorizationHeaderKey    = "Authorization"
+	authorizationHeaderParam  = "Authorization"
 	authorizationPrefix       = "OAuth " // trailing space is intentional
 	oauthConsumerKeyParam     = "oauth_consumer_key"
 	oauthNonceParam           = "oauth_nonce"
@@ -59,7 +59,7 @@ func (s *Signer) SetRequestTokenAuthHeader(req *http.Request) error {
 func (s *Signer) SetAccessTokenAuthHeader(req *http.Request, requestToken *RequestToken, verifier string) error {
 	oauthParams := basicOAuthParams(s.config.ConsumerKey)
 	oauthParams[oauthTokenParam] = requestToken.Token
-	oauthParams[oauthVersionParam] = verifier
+	oauthParams[oauthVerifierParam] = verifier
 	signatureBase, err := signatureBase(req, oauthParams)
 	if err != nil {
 		return err
@@ -101,7 +101,7 @@ func basicOAuthParams(consumerKey string) map[string]string {
 // and sets the header on the Request.
 func setAuthorizationHeader(req *http.Request, oauthParams map[string]string) {
 	authHeader := getAuthHeader(oauthParams)
-	req.Header.Set(authorizationHeaderKey, authHeader)
+	req.Header.Set(authorizationHeaderParam, authHeader)
 }
 
 // getAuthHeader combines the OAuth1 protocol parameters into an authorization