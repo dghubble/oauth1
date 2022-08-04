ec00de17d9b08b7e3d8fa69166befb2911c895b9
RaymondChia <raymondchia@rayark.com>
2022-08-04 14:15:40 +0800

    Add more tests

--- verifier_test.go
+++ verifier_test.go
@@ -4,6 +4,7 @@ import (
 	"crypto"
 	"crypto/x509"
 	"encoding/base64"
+	"fmt"
 	"net/http"
 	"net/url"
 	"strings"
@@ -106,6 +107,42 @@ func TestVerifier_RSA(t *testing.T) {
 	require.NoError(t, err)
 }
 
+func TestVerifier_InvalidRSA(t *testing.T) {
+	// example from TestCollectParameters
+	cases := []string{
+		// invalid signature
+		// first N -> n
+		"nu%2B7FzqMw%2B18w6%2BzcT45SjWBXWjvf%2FW8adgIpgNahfZGzSExrIA6YRugfngCD97t4ms%2B4Vo2ozPOYHhxq%2BIF3EqoSdno5v53rA9mBvOmNU9XKr7gb92F0MVw%2F6M8MQUhputsUW4L7JixEXHymQUEub82ZC58xHJHklPUNIUtmyuxpzeII7E2K09KLMDp9%2F4ne%2FIm%2FufSoWDCBWn9497SIYZKNGyDAHav9zuXFy8x%2FItwknSpvSGG5zr1j2OyaZz7P5AIHVYPryi1N0Mwu35QHES4pafc0z1Z%2Fgm8PMvcI2BofqdEHbs65okhrE%2BSCxPRqJtc1k4A5LkmWbyp91WqHw%3D%3D",
+		// not percent encoded
+		"Nu%%B7FzqMw%2B18w6%2BzcT45SjWBXWjvf%2FW8adgIpgNahfZGzSExrIA6YRugfngCD97t4ms%2B4Vo2ozPOYHhxq%2BIF3EqoSdno5v53rA9mBvOmNU9XKr7gb92F0MVw%2F6M8MQUhputsUW4L7JixEXHymQUEub82ZC58xHJHklPUNIUtmyuxpzeII7E2K09KLMDp9%2F4ne%2FIm%2FufSoWDCBWn9497SIYZKNGyDAHav9zuXFy8x%2FItwknSpvSGG5zr1j2OyaZz7P5AIHVYPryi1N0Mwu35QHES4pafc0z1Z%2Fgm8PMvcI2BofqdEHbs65okhrE%2BSCxPRqJtc1k4A5LkmWbyp91WqHw%3D%3D",
+		// not base64 encoded
+		// last = is replaced
+		"Nu%2B7FzqMw%2B18w6%2BzcT45SjWBXWjvf%2FW8adgIpgNahfZGzSExrIA6YRugfngCD97t4ms%2B4Vo2ozPOYHhxq%2BIF3EqoSdno5v53rA9mBvOmNU9XKr7gb92F0MVw%2F6M8MQUhputsUW4L7JixEXHymQUEub82ZC58xHJHklPUNIUtmyuxpzeII7E2K09KLMDp9%2F4ne%2FIm%2FufSoWDCBWn9497SIYZKNGyDAHav9zuXFy8x%2FItwknSpvSGG5zr1j2OyaZz7P5AIHVYPryi1N0Mwu35QHES4pafc0z1Z%2Fgm8PMvcI2BofqdEHbs65okhrE%2BSCxPRqJtc1k4A5LkmWbyp91WqHw%3D%3A",
+	}
+	for _, signature := range cases {
+		values := url.Values{}
+		values.Add("c2", "")
+		values.Add("plus", "2 q") // duplicate keys not supported, a3 -> plus
+		req, err := http.NewRequest("POST", "http://127.0.0.1:50428/request?b5=%3D%253D&a3=a&c%40=&a2=r%20b", strings.NewReader(values.Encode()))
+		assert.NoError(t, err)
+		req.Header.Set(contentType, formContentType)
+		header := `OAuth oauth_consumer_key="consumer_key", oauth_nonce="6hrVr5eVPa5cWUtnW3sRIMlti2uB0zM43pk9mYIggFY%3D", oauth_signature="` + signature + `", oauth_signature_method="RSA-SHA1", oauth_timestamp="1659497715", oauth_token="", oauth_version="1.0"`
+		req.Header.Set(authorizationHeaderParam, header)
+
+		err = NewVerifierManager(func(consumerKey, method string, params map[string]string) (Verifier, error) {
+			assert.Equal(t, "consumer_key", consumerKey)
+			assert.Equal(t, "RSA-SHA1", method)
+
+			b, err := base64.StdEncoding.DecodeString(rsaKey)
+			require.NoError(t, err)
+			k, err := x509.ParsePKCS1PrivateKey(b)
+			require.NoError(t, err)
+			return NewRSAVerifier(&k.PublicKey, crypto.SHA1), nil
+		}, "", -1).Verify(req)
+		require.Error(t, err)
+	}
+}
+
 func TestVerifier_HMACSHA1_WithoutOAuthToken(t *testing.T) {
 	// example from TestCollectParameters
 	values := url.Values{}
@@ -144,6 +181,24 @@ func TestVerifier_HMACSHA1_WithOAuthToken(t *testing.T) {
 	require.NoError(t, err)
 }
 
+func TestVerifier_InvalidHMACSHA1(t *testing.T) {
+	// example from TestCollectParameters
+	values := url.Values{}
+	values.Add("c2", "")
+	values.Add("plus", "2 q")
+	req, err := http.NewRequest("POST", "http://127.0.0.1:51060/request?b5=%3D%253D&a3=a&c%40=&a2=r%20b", strings.NewReader(values.Encode()))
+	assert.NoError(t, err)
+	req.Header.Set(contentType, formContentType)
+	req.Header.Set(authorizationHeaderParam, `OAuth oauth_consumer_key="9djdj82h48djs9d2", oauth_nonce="UrmFlgNMjd2UF8sodAzDPqN5AylKo33kxF9gqnd1j7E%3D", oauth_signature="3ZYe7rg2We2jgfv20ZNqlVbCibY%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1659507521", oauth_token="", oauth_version="1.0"`)
+
+	err = NewVerifierManager(func(consumerKey, method string, params map[string]string) (Verifier, error) {
+		assert.Equal(t, "9djdj82h48djs9d2", consumerKey)
+		assert.Equal(t, "HMAC-SHA1", method)
+		return NewHMACVerifier(NewConfig("9djdj82h48djs9d2", "j49sk3j29djd"), ""), nil
+	}, "", -1).Verify(req)
+	require.Error(t, err)
+}
+
 func TestVerifier_TooOld(t *testing.T) {
 	// example from TestCollectParameters
 	values := url.Values{}
@@ -161,5 +216,23 @@ func TestVerifier_TooOld(t *testing.T) {
 		assert.Equal(t, "HMAC-SHA1", method)
 		return NewHMACVerifier(NewConfig("9djdj82h48djs9d2", "j49sk3j29djd"), "dh893hdasih9"), nil
 	}, "", time.Hour).Verify(req)
+	require.Error(t, err)
+}
+
+func TestVerifier_RejectedByGetVerifier(t *testing.T) {
+	// example from TestCollectParameters
+	values := url.Values{}
+	values.Add("c2", "")
+	values.Add("plus", "2 q")
+	req, err := http.NewRequest("POST", "/request?b5=%3D%253D&a3=a&c%40=&a2=r%20b", strings.NewReader(values.Encode()))
+	assert.NoError(t, err)
+	req.Header.Set(contentType, formContentType)
+	req.Header.Set(authorizationHeaderParam, `OAuth oauth_consumer_key="9djdj82h48djs9d2", oauth_nonce="FnwtgC3exdLhc2Kspuc9GYPhGzgyQEB1T5tRcfM2FtM%3D", oauth_signature="6F8L5pN4iERKpwRqfuMGA9WesYU%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1659507394", oauth_token="kkk9d7dh3k39sjv7", oauth_version="1.0"`)
+	req.Header.Set("Forwarded", `for="127.0.0.1:51043";proto=http`)
+	req.Host = "127.0.0.1:51043"
+
+	err = NewVerifierManager(func(consumerKey, method string, params map[string]string) (Verifier, error) {
+		return nil, fmt.Errorf("fail to get verifier")
+	}, "", -1).Verify(req)
 	require.Error(t, err)
 }
