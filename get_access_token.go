4112d153434b34d4cd8a600070c5a87c19eb25df
David Calle <davidfcalle@gmail.com>
2023-06-15 16:00:57 +0200

    remove fatal

--- get_access_token.go
+++ get_access_token.go
@@ -2,7 +2,8 @@ package oauth1
 
 import (
 	"fmt"
-	"log"
+	"net/http"
+	"net/http/httputil"
 )
 
 func GetAccessToken(consumerKey, consumerSecret, oauthVerifier, baseURL string) (string, string, error) {
@@ -24,7 +25,7 @@ func GetAccessToken(consumerKey, consumerSecret, oauthVerifier, baseURL string) 
 	// Get request token
 	requestToken, requestSecret, err := config.RequestToken()
 	if err != nil {
-		log.Fatalf("Error obtaining request token: %v", err)
+		fmt.Errorf("Error obtaining request token: %v", err)
 	}
 	fmt.Printf("Request Token: %s\n", requestToken)
 	fmt.Printf("Request Token Secret: %s\n", requestSecret)
@@ -32,7 +33,7 @@ func GetAccessToken(consumerKey, consumerSecret, oauthVerifier, baseURL string) 
 	// Exchange request token and OAuth verifier for an access token
 	accessToken, accessSecret, err := config.AccessToken(requestToken, requestSecret, oauthVerifier)
 	if err != nil {
-		log.Fatalf("Error obtaining access token: %v", err)
+		fmt.Errorf("Error obtaining access token: %v", err.Error())
 	}
 	fmt.Printf("Access Token: %s\n", accessToken)
 	fmt.Printf("Access Token Secret: %s\n", accessSecret)
@@ -43,11 +44,25 @@ func GetAccessToken(consumerKey, consumerSecret, oauthVerifier, baseURL string) 
 	httpClient = config.Client(NoContext, token)
 
 	// Example API request
-	resp, err := httpClient.Get(baseURL + "/api/rest/products")
-	if err != nil {
-		log.Fatalf("Error making API request: %v", err)
+	resp, err := httpClient.Get(baseURL + "/rest/V1/store/storeConfigs")
+	if err == nil {
+		prettyPrintHTTPResponse(resp)
+	} else {
+		fmt.Errorf("Error making API request: %v", err.Error())
 	}
 	defer resp.Body.Close()
 
 	return accessToken, accessSecret, nil
+}
+
+func prettyPrintHTTPResponse(response *http.Response) {
+	// Dump the response as a byte slice
+	dump, err := httputil.DumpResponse(response, true)
+	if err != nil {
+		fmt.Println("Failed to dump response:", err)
+		return
+	}
+
+	// Print the formatted response
+	fmt.Println(string(dump))
 }
