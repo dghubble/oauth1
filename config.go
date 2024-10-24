39423d0a03abe148fd3fbf2ac2618bc59082b8db
Gawen Arab <g@wenarab.com>
2024-10-24 14:58:04 +0200

    Accept `context.Context`

--- config.go
+++ config.go
@@ -65,8 +65,8 @@ func NewClient(ctx context.Context, config *Config, token *Token) *http.Client {
 // oauth_callback_confirmed is true. Returns the request token and secret
 // (temporary credentials).
 // See RFC 5849 2.1 Temporary Credentials.
-func (c *Config) RequestToken() (requestToken, requestSecret string, err error) {
-	req, err := http.NewRequest("POST", c.Endpoint.RequestTokenURL, nil)
+func (c *Config) RequestToken(ctx context.Context) (requestToken, requestSecret string, err error) {
+	req, err := http.NewRequestWithContext(ctx, "POST", c.Endpoint.RequestTokenURL, nil)
 	if err != nil {
 		return "", "", err
 	}
@@ -144,8 +144,8 @@ func ParseAuthorizationCallback(req *http.Request) (requestToken, verifier strin
 // Endpoint AccessTokenURL. Returns the access token and secret (token
 // credentials).
 // See RFC 5849 2.3 Token Credentials.
-func (c *Config) AccessToken(requestToken, requestSecret, verifier string) (accessToken, accessSecret string, err error) {
-	req, err := http.NewRequest("POST", c.Endpoint.AccessTokenURL, nil)
+func (c *Config) AccessToken(ctx context.Context, requestToken, requestSecret, verifier string) (accessToken, accessSecret string, err error) {
+	req, err := http.NewRequestWithContext(ctx, "POST", c.Endpoint.AccessTokenURL, nil)
 	if err != nil {
 		return "", "", err
 	}
